dc3ea0fdb95b496918fc675fcccb2e25d946787c
Kuba663 <kuba63b@gmail.com>
2023-04-08 09:40:52 +0200

    fix(memory): Changed precompiled header file

--- xallocator/Allocator.cpp
+++ xallocator/Allocator.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "pch.h"
 
 #include "Allocator.h"
 #include "DataTypes.h"
--- xallocator/Fault.cpp
+++ xallocator/Fault.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "pch.h"
 
 #include "Fault.h"
 #include "DataTypes.h"
