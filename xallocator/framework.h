ebd89656eb6f7f6349d17f23542a6b458b4b89a4
Kuba663 <kuba63b@gmail.com>
2023-04-08 09:41:16 +0200

    refactor(memory): Some windows related macro

--- xallocator/framework.h
+++ xallocator/framework.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#ifdef WIN32
 #define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
 // Pliki nagłówkowe systemu Windows
 #include <windows.h>
+#endif
